cef4b06d94caa3d05356fd30e896b63b7ed9a99b
RoninsWork <yurkaronin@gmail.com>
2022-10-06 12:06:13 +0300

    Поправил отступы в слайдере

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,7 @@
 const swiper = new Swiper('.js-slider', {
-  // Optional parameters
-  // slidesPerView: 3,
-  // spaceBetween: 32,
+  spaceBetween: 32,
   loop: true,
 
-  // Navigation arrows
   navigation: {
     nextEl: '.js-button-next',
     prevEl: '.js-button-prev',
@@ -14,19 +11,14 @@ const swiper = new Swiper('.js-slider', {
   },
 
   breakpoints: {
-    // when window width is >= 320px
     376: {
       slidesPerView: 1,
-      spaceBetween: 16
     },
-    // when window width is >= 480px
     426: {
       slidesPerView: 2,
-      spaceBetween: 28
     },
     600: {
       slidesPerView: 3,
-      spaceBetween: 32
     }
   }
 
